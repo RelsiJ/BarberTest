787c72d584be619a49759530cf9f9011de700842
RelsiJ <theonlypas55@gmail.com>
2026-05-04 23:35:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,23 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://barber_user:QbehjfHWOePZgf8kuAczQsNI0sjed5RP@dpg-d7rr67n7f7vs73d78dgg-a/barbershop_db_0r9t')
 conn = psycopg2.connect(DATABASE_URL)
- 
+
+with conn.cursor() as cur:
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS clients (
+            id SERIAL PRIMARY KEY,
+            phone VARCHAR(20),
+            name VARCHAR(100),
+            booking DATE,
+            time TIME,
+            barber VARCHAR(30),
+            service VARCHAR(30),
+            created_at TIMESTAMP DEFAULT NOW()
+        )
+    """)
+    conn.commit()
+    print("✅ Таблица clients готова")
+
 app = Flask(__name__) 
 
 
