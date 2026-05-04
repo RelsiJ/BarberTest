9e06e0d5866dda74ad6887a3f56bdd718bcb6818
RelsiJ <theonlypas55@gmail.com>
2026-05-04 19:18:45 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request 
+import psycopg2
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://barber_user:QbehjfHWOePZgf8kuAczQsNI0sjed5RP@dpg-d7rr67n7f7vs73d78dgg-a/barbershop_db_0r9t')
+conn = psycopg2.connect(DATABASE_URL)
+ 
+app = Flask(__name__) 
+
+
+gc = gspread.service_account(filename="barbershop-api-494914-1546df79a4ce.json")
+sheet = gc.open("BarberShop записи").sheet1
+def add_to_sheets(phone, name, booking, time, barber, service):
+    cur = conn.cursor()
+    cur.execute("SELECT COALESCE(MAX(id), 0) FROM clients")
+    max_id = cur.fetchone()[0]
+    cur.close()
+    new_id = max_id + 1
+    sheet.append_row([new_id, phone, name, booking, time, barber, service])
+
+
+@app.route('/') 
+def home(): 
+    return 'OK' 
+ 
+@app.route('/submit', methods=['POST']) 
+def submit(): 
+
+    name = request.form.get('name') 
+    phone = request.form.get('phone') 
+    booking = request.form.get('booking_date') 
+    time = request.form.get('time') 
+    barber = request.form.get('barber') 
+    service = request.form.get('service') 
+ 
+    print(name, phone, booking, time, barber, service) 
+ 
+    cur = conn.cursor() 
+    cur.execute("INSERT INTO clients (phone, name, booking, time, barber, service) VALUES (%s, %s, %s, %s, %s, %s)", (phone, name, booking, time, barber, service)) 
+    conn.commit() 
+    cur.close() 
+
+    add_to_sheets(phone, name, booking, time, barber, service)
+    print(f"Запись добавлена: {name}, {phone}" )
+ 
+    return 'OK' 
+ 
+if __name__ == '__main__': 
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
